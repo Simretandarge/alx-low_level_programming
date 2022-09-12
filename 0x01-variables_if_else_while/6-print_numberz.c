15d9a1f94d414be49e6fdab654adc4301a2b2831
simret mekuria <andargesimret@gmail.com>
2022-09-12 04:26:53 -0700

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers since 0 to 9 
+ * main - Prints the numbers since 0 to 9
  *
  * Return: Always (Success)
  */
@@ -18,5 +18,3 @@ int main(void)
 
 	return (0);
 }
-
-
