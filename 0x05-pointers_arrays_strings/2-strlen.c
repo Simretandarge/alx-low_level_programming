d6c73c07b01efe4a7de8550c4826e6ff51812d8c
simret mekuria <andargesimret@gmail.com>
2022-09-19 06:51:01 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
+ *_strlen - returns the length of a string
  * @s: string
- * Return: returns length as integer; 
+ *Return: returns length as integer; 
  */
 
 int _strlen(char *s)
