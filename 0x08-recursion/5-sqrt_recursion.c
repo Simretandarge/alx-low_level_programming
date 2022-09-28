e8483ffee5ac028cb501606c07d5809d05a7009b
simret mekuria <andargesimret@gmail.com>
2022-09-28 07:24:00 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
 		return (n);
-	return (sqrt(0, n));
+	return (_sqrt(0, n));
 }
 
 /**
@@ -22,9 +22,9 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int n, int x)
 {
-	if (n > x/2)
+	if (n > x / 2)
 		return (-1);
 	else if (n * n == x)
 		return (n);
-	return (sqrt(n + i, x));
+	return (_sqrt(n + i, x));
 }
