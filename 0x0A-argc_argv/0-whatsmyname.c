63e4a6669dc7b772dfa0d1f0c721e71a0f7f63f7
simret mekuria <andargesimret@gmail.com>
2022-09-30 05:45:54 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main."
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argy: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) arge;
+       	printf("%s\n", argv[0]);
+	
+	return (0);
+}
