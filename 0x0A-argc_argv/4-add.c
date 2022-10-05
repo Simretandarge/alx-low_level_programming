25365801627dc6515c95615498ddebd54f020d88
simret mekuria <andargesimret@gmail.com>
2022-10-05 06:37:26 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
 
 /**
- * main - Prints coints left
- * @args: Paramater Entry
- * @argv: one number of argumets
- * Return: zero onone number of argumetsr
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
-int main(int args, char *argv[])
-{
-	int C, coins = 0;
 
-	if (args != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	C = atoi(argv[1]);
-	if (c < 0)
-	{
+int main(int argc, char *argv[])
+{
+	int i, j, length, sum;
+	char *ptr;
+
+	if (argc < 2)
 		printf("0\n");
-		return (0);
-	}
-	for (; C >= 0;)
+	else
 	{
-		if (c >= 25)
-			c -= 25;
-		else if (c >= 10)
-			C -= 10;
-		else if (c >= 5)
-			C -= 5;
-		else if (c >= 2)
-			C -= 2;
-		else if (c >= 1)
-			c -= 1;
-		else
-			break;
-		coins += 1;
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
 	}
-	printf("%d\n", coins)
-		return (0);
+	return (0);
 }
