a135e4e50ec79061a896f74448884613d2fe13ab
simret mekuria <andargesimret@gmail.com>
2023-04-12 11:19:27 -0700

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,26 +1,42 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
+ * interpolation_search - searches for a value in an array of integers
  *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: Always EXIT_SUCCESS
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the first where value is located or -1 on failure or not found
  */
-int linear_search(int *array, size_t size, int value)
+
+int interpolation_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i = 0, low = 0, high = size - 1;
 
-	if (array == NULL)
-		return (-1);
-
-	for (i = 0; i < (int)size; i++)
+	if (array)
 	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
-			return (i);
+		while ((value >= array[low]))
+		{
+			i = low + (((double)(high - low) /
+						(array[high] - array[low])) * (value - array[low]));
+
+			if (i > size)
+			{
+				printf("Value checked array[%lu] is out of range\n", i);
+				break;
+			}
+
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+			if (array[i] < value)
+				low = i + 1;
+			else if (array[i] > value)
+				high = i - 1;
+			else
+				return (i);
+		}
 	}
+
 	return (-1);
 }
